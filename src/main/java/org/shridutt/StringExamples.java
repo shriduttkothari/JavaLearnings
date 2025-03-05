7baf962fe9e2b3bff8d2b35670c994966d85f8b1
Shridutt Kothari <shriduttkothari@gmail.com>
2025-03-05 16:48:26 +0100

    Using StringBuilder as this is single thread program

--- src/main/java/org/shridutt/StringExamples.java
+++ src/main/java/org/shridutt/StringExamples.java
@@ -7,7 +7,7 @@ public class StringExamples {
         if(input.isBlank()) return input;
         else {
             char[] inputCharArray = input.toCharArray();
-            StringBuffer output = new StringBuffer();
+            StringBuilder output = new StringBuilder();
             for (int index = inputCharArray.length - 1; index >= 0; index--) {
                 output.append(inputCharArray[index]);
             }
