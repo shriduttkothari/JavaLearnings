2d7d8bacc28f8fe4f8550c5ba7530903ccd9811e
Shridutt Kothari <shriduttkothari@gmail.com>
2025-03-14 16:23:24 +0100

    Implement the solution

--- src/main/java/org/shridutt/ds/patterns/slidingwindow/LongestSubstringLength.java
+++ src/main/java/org/shridutt/ds/patterns/slidingwindow/LongestSubstringLength.java
@@ -1,5 +1,8 @@
 package org.shridutt.ds.patterns.slidingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given a string, find the length of the longest substring which has no repeating characters.
  *
@@ -24,13 +27,28 @@ package org.shridutt.ds.patterns.slidingwindow;
 public class LongestSubstringLength {
 
     public int longestSubstringLength(String input) {
-        if(input == null || input.length() == 0) {
+        if (input == null || input.length() == 0) {
             throw new IllegalArgumentException("Invalid input");
         }
 
         int longestSubstringLength = Integer.MIN_VALUE;
+        int leftIndex = 0;
+        StringBuilder subStringWindow = new StringBuilder();
+        Map<Character, Integer> characterFrequency = new HashMap<>();
 
+        for (int rightIndex = 0; rightIndex < input.length(); rightIndex++) {
+            subStringWindow.append(input.charAt(rightIndex));
+            characterFrequency.put(input.charAt(rightIndex), characterFrequency.getOrDefault(input.charAt(rightIndex), 0) +1); //increment the frequency of char at rightIndex
 
+            while(characterFrequency.getOrDefault(input.charAt(rightIndex), 0) > 1) { //condition which we don't want to allow
+                longestSubstringLength = Math.max(longestSubstringLength, (rightIndex-leftIndex)); 
+                characterFrequency.put(input.charAt(rightIndex), characterFrequency.getOrDefault(input.charAt(rightIndex), 0) -1);
+                leftIndex++;
+                if (characterFrequency.get(input.charAt(leftIndex)) == 0) {
+                    characterFrequency.remove(input.charAt(leftIndex));
+                }
+            }
+        }
         return longestSubstringLength != Integer.MIN_VALUE ? longestSubstringLength : 0;
     }
 }
